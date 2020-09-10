ad9b83e999a22655c0453aafe89bac71e7cd6498
Elizabeth Adegbaju <adeotunadegbaju@gmail.com>
2020-09-10 01:39:31 +0100

    update settings.py remove spaces

--- travelcave/settings.py
+++ travelcave/settings.py
@@ -77,7 +77,6 @@ WSGI_APPLICATION = 'travelcave.wsgi.application'
 
 # Summernote
 SUMMERNOTE_THEME = 'lite'
-
 SUMMERNOTE_CONFIG = {
     'summernote': {
         'width': '100%',
@@ -136,6 +135,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
+
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
